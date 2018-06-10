ab34f960ebb9c9d52a89d779bd37451d83b38d04
Karl Semich <0xloem@gmail.com>
2018-06-10 16:40:45 +0000

    tincanterm: details for using avago transmitters i ordered

---
+++ tincanterm/examples/config.arduino.avagosfh.h
@@ -0,0 +1,55 @@
+#pragma once
+
+/*
+ * Arduino sending/receiving via handmade fiber optic cables using a AVAGO TRANSMITTERS AND RECEIVERS.
+ * I used the SFH757V and SFH250V listed in hardware.txt, which made assembly very easy.
+ * They can operate at speeds far in excess of what this system can handle, but the values here add a few milliseconds of settle time
+ * to allow for mistakes or simplifications (my system ran fine even without any covering on the fiber).
+ *
+ * A length of cable is optionally surrounded in heat shrink tubing or any other opaque material.
+ * One end is connected to the SFH757V and the other to the SFH250V.
+ *
+ * The light is emitted when the signal is low, because I have heard that microcontroller pins are better at sinking current than sourcing it.
+ *
+ * Receiver SFH250V wiring:
+ *    Back left leg connects to GROUND
+ *    Back right leg connects to ARDUINO PIN 4
+ *    Back right leg additionally connects to a 100k Ohm resistor, which connects to +5V
+ *
+ * Transmitter SFH757V wiring:
+ *    Back left leg connects to +5V
+ *    Back right leg connects to a 100 Ohm resistor, which connects to ARDUINO PIN 5
+ */
+
+
+// which implementation to use
+#define IMPL        ARDUINO
+
+// identifier for input port
+#define INPUT_PORT  4
+
+// identifier for output port
+#define OUTPUT_PORT 5
+
+// Character to use for linebreak
+#define LINEBREAK '\n'
+
+// milliseconds to spend sending each bit (inverse baud in ms)
+#define BIT_MS 5
+
+// delay to make sure bit value has settled when reading
+#define SETTLE_MS 3
+
+// pull resistor direction for input port
+#define INPUT_PULL  DOWN
+
+// set to nonzero if input should be set output-low before reading
+// this drains stored charge
+#define INPUT_DRAIN 0
+
+// baud rate for local communications, if applicable
+// note that if the device has no flow control the buffer will quickly fill, causing data loss unless rate is limited elsewhere
+#define LOCAL_BAUD 9600
+
+// unset to enable debugging output
+#define NDEBUG 1
---
+++ tincanterm/examples/config.arduino.photoresistor.h
@@ -0,0 +1,58 @@
+#pragma once
+
+/*
+ * Arduino sending/receiving via handmade fiber optic cables using a PHOTORESISTOR.
+ * Photoresistors are very slow, so communication is like molasses.
+ *
+ * A length of cable is surrounded in heat shrink tubing or any other opaque material.
+ * An LED is affixed to one end.  The LED is tested that it creates significant light coming out of the opposite end of the cable.
+ * A photoresistor is then affixed to this opposite end.
+ *
+ * Dark heat shrink tubing or some other opaque material must be placed around connections to prevent external light from entering.
+ *
+ * Photoresistor wiring:
+ *    First leg connects to +5V
+ *    Second leg connects to ARDUINO PIN 4
+ *    Second leg additionally connects to two 33K Ohm resistors wired in series (end-to-end), which connect to GROUND
+ *      This high-resistance connection to ground acts as pull-down on the arduino pin, making a voltage divider with the photoresistor.
+ *      The resistance may need to be increased if the system reads LOW/FALSE/0 when it should not
+ *      The resistance may need to be decreased if the system reads HIGH/TRUE/1 when it should not
+ *      This can be checked by commenting the NDEBUG definition at the bottom of this file.
+ *
+ * LED wiring:
+ *    Negative leg connects to GROUND
+ *    Positive leg connects to a 330 Ohm resistor, which connects to ARDUINO PIN 5
+ */
+
+
+// which implementation to use
+#define IMPL        ARDUINO
+
+// identifier for input port
+#define INPUT_PORT  4
+
+// identifier for output port
+#define OUTPUT_PORT 5
+
+// Character to use for linebreak
+#define LINEBREAK '\n'
+
+// milliseconds to spend sending each bit (inverse baud in ms)
+#define BIT_MS 29
+
+// delay to make sure bit value has settled when reading
+#define SETTLE_MS 13
+
+// pull resistor direction for input port
+#define INPUT_PULL  DOWN
+
+// set to nonzero if input should be set output-low before reading
+// this drains stored charge
+#define INPUT_DRAIN 0
+
+// baud rate for local communications, if applicable
+// note that if the device has no flow control the buffer will quickly fill, causing data loss unless rate is limited elsewhere
+#define LOCAL_BAUD 9600
+
+// unset to enable debugging output
+#define NDEBUG 1
