d71733f1fcd330c66d0ece4f71cf35a4494dce94
0xloem <0xloem@gmail.com>
2021-05-19 16:06:26 -0400

    working on build errors

--- gr-blocks/python/bindings/python_bindings.cc
+++ gr-blocks/python/bindings/python_bindings.cc
@@ -22,6 +22,7 @@ namespace py = pybind11;
 /**************************************/
 // BINDING_FUNCTION_PROTOTYPES(
     void bind_pigpio_sink(py::module& m);
+    void bind_pad_crop(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -51,5 +52,6 @@ PYBIND11_MODULE(openemissions_python, m)
     /**************************************/
     // BINDING_FUNCTION_CALLS(
     bind_pigpio_sink(m);
+    bind_pad_crop(m);
     // ) END BINDING_FUNCTION_CALLS
 }
